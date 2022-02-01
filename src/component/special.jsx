007c6185694a87b741335cd5d4acd9d1535f2247
Amatid <Dimagrodno@yandex.ru>
2022-02-01 21:45:41 +0300

    finishe component special

--- src/component/special.jsx
+++ src/component/special.jsx
@@ -3,8 +3,34 @@ import classes from './special.module.css'
 function Special() {
     return (
         <div className={classes.special}>
-            <div className={classes.about}>asd</div>
-            <div className={classes.offer}>asddas</div>
+            <div>
+                <div className={classes.about}>
+                    <p>Order</p>
+                    <p>Flowers</p>
+                    <div></div>
+                    <p>Shop Our Collection</p>
+                    <div className={classes.circle}></div>
+                </div>
+                <div className={classes.about}>
+                    <p>Corporate</p>
+                    <p>Flowers</p>
+                    <div></div>
+                    <p>See Our Work</p>
+                    <div className={classes.circle}></div>
+                </div>
+                <div className={classes.about}>
+                    <p>Hadcrafted</p>
+                    <p>Wedding</p>
+                    <div></div>
+                    <p>View Our Portfolio</p>
+                    <div className={classes.circle}></div>
+                </div>
+            </div>
+            <div className={classes.offer}>
+                <p>Order <span>Flowers</span></p>
+                <p>100% Moneyback Guarantee</p>
+                <button>Shop Now {'>'}</button>
+            </div>
         </div>
     );
 }
